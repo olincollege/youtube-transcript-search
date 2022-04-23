4b804e835e7d9a6418f931739e6beee7238991a0
Brooke Moss <me@brookem.dev>
2022-04-23 17:54:23 -0400

    Trying to figure out pylint stuff

--- load_files.py
+++ load_files.py
@@ -1,6 +1,9 @@
+"""_summary_
+"""
+
 import json
 
 with open('transcript_data/Tapakapa/Cars_Are_Not_the_Problem.json') as video_file:
     video_data = json.load(video_file)
-    
+
     print(video_data)
--- starter_code.py
+++ starter_code.py
@@ -1,14 +1,18 @@
-from keys import *
+"""_summary_
+"""
 
 from youtube_channel_transcript_api import YoutubeChannelTranscripts
+from keys import youtube_api_key
+
 # import csv
 
-channel_name = 'Tapakapa'
+CHANNEL_NAME = 'Tapakapa'
 
-channel_getter = YoutubeChannelTranscripts(channel_name, youtube_api_key)
+channel_getter = YoutubeChannelTranscripts(CHANNEL_NAME, youtube_api_key)
 
 # videos_data, videos_errored = channel_getter.get_transcripts()
 
-videos_errored = channel_getter.write_transcripts(f'transcript_data/{channel_name}/', just_text=True)
-# videos_errored = channel_getter.write_transcripts(f'transcript_data/{channel_name}/')
-
+videos_errored = channel_getter.write_transcripts( \
+    f'transcript_data/{CHANNEL_NAME}/', just_text=True)
+# videos_errored = channel_getter.write_transcripts(f' \
+# transcript_data/{CHANNEL_NAME}/')
