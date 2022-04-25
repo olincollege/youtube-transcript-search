eafe0dcf72c8b78ffc948a84689a40ef1b7d69d8
Kat Canavan <katcanavan@gmail.com>
2022-04-25 12:38:01 -0400

    Rename files

--- model.py
+++ model.py
@@ -20,3 +20,15 @@ class Video():
 
     def __init__(self, filepath):
         self.path = filepath
+
+
+class YTSearchModel():
+
+    def __init__(self):
+        pass
+
+    def ingest_jsons(self):
+        pass
+
+    def pull_transcripts(self):
+        pass
