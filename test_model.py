36b554a4c9d2a39843463356350cef4d374583d7
Brooke Moss <me@brookem.dev>
2022-05-06 17:37:08 -0400

    Adds test scaffolding and first test function

---
+++ test_model.py
@@ -0,0 +1,11 @@
+
+import pytest
+from model import Video, Channel, YTSearchModel
+
+
+def test_update_search():
+    pass
+
+def test_search():
+    pass
+
---
+++ test_view.py
@@ -0,0 +1,30 @@
+
+import pytest
+from view import ViewTerminal
+
+view_test = ViewTerminal()
+
+
+
+def test_get_search_input():
+    pass
+
+def test_draw_results():
+    pass
+
+
+
+@pytest.mark.parametrize(
+    ["inputs_list", "end_result"],
+    [
+        (["y"], "y"),
+        (["n"], "n"),
+        (["hello", "y"], "y")
+    ]
+)
+
+def test_search_again(monkeypatch, inputs_list, end_result):
+
+    inputs = iter(inputs_list)
+    monkeypatch.setattr('builtins.input', lambda msg: next(inputs))
+    assert view_test.search_again() == end_result
