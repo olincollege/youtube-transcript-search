f0b4bb2ad0cca68ca2d9f2e85265d6c03bfc38b9
Brooke Moss <me@brookem.dev>
2022-05-06 18:32:29 -0400

    Test framework for model init

--- test_model.py
+++ test_model.py
@@ -2,10 +2,38 @@
 import pytest
 from model import Video, Channel, YTSearchModel
 
+@pytest.mark.parametrize(
+    # parameters = [current_channel_name, keywords, available_channels]
+    ["parameters", "channels", "results"],
+    [
+        (
+            [
+                "Kat Canavan",      # Current channel name
+                "robots, nasa",     # Keywords
+                ["Kat Canavan"]     # Available channels (list)
+            ],
+            {                       # Dictionary of channel objects
+                "Kat Canavan": Channel("Kat Canavan"),# Only using keys for test
+            },
+            [                       # List of results tuples
+                (),
+            ]
+        )
+    ]
+)
 
-def test_update_search():
-    pass
+def test_YTSearchModel_init(parameters, channels, results):
+    model_test = YTSearchModel(parameters[0], parameters[1], parameters[2])
 
-def test_search():
-    pass
+    assert model_test.channels.keys() == channels.keys()
+    assert model_test.results == results
+
+# def test_update_search(parameters, channels, results):
+    # model_test = YTSearchModel(parameters[0], parameters[1], parameters[2])
+
+    # assert model_test.
+    # pass
+
+# def test_search():
+#     pass
 
